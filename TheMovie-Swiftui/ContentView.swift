be9c30c34ad64b959641f0c32cae5701959f6ef8
imran <imufun2@gmail.com>
2019-09-15 14:14:39 +0600

    Initial Commit

---
+++ TheMovie-Swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMovie-Swiftui
+//
+//  Created by Imran on 15/9/19.
+//  Copyright © 2019 Imran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
